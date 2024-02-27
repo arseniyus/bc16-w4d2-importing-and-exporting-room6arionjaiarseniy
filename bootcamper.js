4a8e93e0416d1330d42c7eb0fffd7cde74d835ca
arseniyus <r.kozachenko2000@gmail.com>
2024-02-27 11:18:00 +0000

    2b started

---
+++ bootcamper.js
@@ -0,0 +1,18 @@
+// Make an introduce function
+//
+// Pull the data from the json
+// 
+
+
+function introduce(); {
+    if pets = true 
+    console.log (`Hi, my name is ${name} I am ${age} years old and I am a per owner`)
+
+    else console.log (`Hi, my name is ${name} I am ${age} years old and I am not a pet owner`)
+    
+}
+
+
+
+
+
