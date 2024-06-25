d4dfdeab92af400cd2e7d1be5dbcb6ca039501b7
arseniyus <r.kozachenko2000@gmail.com>
2024-06-25 17:35:40 +0100

    made an export function

--- bootcampers.js
+++ bootcampers.js
@@ -1,33 +1,27 @@
-// PLAN:
-// Create bootcampers.js file in root of project ✅
-// Inside bootcampers.js create an introduce function ✅
-// Introduce function = takes in a bootcamper object, returns a string back
-// Bootcamper object is string with name, age, pets
-// OR string with name, age and without pets
-// Export the introduce function
-// Import data from bootcampers.json
-// Function takes an object and pulls back a string
+import bootcampers from './bootcampers.json' with { type: "json" };
 
-import bootcampers from './bootcampers.json' with { "type": "json" }
+export function introduce(bootcamper) {
+  const { firstName, lastName, age, hasPets } = bootcamper;
 
-let temporaryObject = {
-  "firstName": "Arion",
-  "lastName": "King",
-  "age": 27,
-  "hasPets": true
+  if (hasPets) {
+    return `Hi, my name is ${firstName} ${lastName}. I'm ${age} years old and I am a pet owner.`; 
+  } else {
+    return `Hi, my name is ${firstName} ${lastName}. I'm ${age} years old and I have no pets.`;
+  }
 }
 
-function introduce(e) {
-  // const { firstName, lastName, age, hasPets } = bootcampers;
+// export bootcampers
+// import botocampers
+// create export function introduce which will console.log this string:
+// ""Hi, my name is John Doe. I'm 32 years old and I have no pets.
+// {$ } for names, ages, and pets.
 
-  if(e.hasPets === true) {
-    return`Hi, my name is ${e.firstName} ${e.lastName}. I'm ${e.age} years old and I'm a pet owner.`
-  }
+console.log(
+  introduce({
+    firstName: "Tom",
+    lastName: "Pauley",
+    age: "29",
+    hasPets: true
+  })
+)
 
-  else {
-    return`Hi, my name is ${e.firstName} ${e.lastName}. I'm ${e.age} years old and I have no pets.`
-  }
-} 
-console.log(introduce(bootcampers[2]));
-
-export default introduce;
